bd8e2d2aae721a0fdc1eb7337d63e3a13576411b
MahalakshmiAnandan <47468528+MahalakshmiAnandan@users.noreply.github.com>
2019-06-12 18:45:44 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include<conio.h>
+void main()
+{
+    int a,b,c;
+    c=a+b;
+    printf("/n Enter the no is %d %d",a,b);
+    scanf("%d %d",&a,&b);
+    printf("/n sum is %d",c);
+    getch();
+}
