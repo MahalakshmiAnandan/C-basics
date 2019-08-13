e57e8e926acdb7e5005489ccd85f380b811b6648
MahalakshmiAnandan <47468528+MahalakshmiAnandan@users.noreply.github.com>
2019-08-13 18:27:09 +0530

    Create avg.c

---
+++ avg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+char name;
+int rollno;
+int m1,m2;
+int avg;
+for(i=0;i<n;i++)
+{
+printf("enter the name");
+scanf("%c",&name);
+printf("enter the roll no");
+scanf("%d",&rollno);
+printf("enter the marks");
+scanf("%d %d",&m1,&m2);
+avg=(m1+m2)/2;
+printf("avg is",avg);
+scanf("%d",&avg);
+}
+}
